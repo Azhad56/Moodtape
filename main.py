2a5c34304ff70965bef4afd3dc5cd60e2f37d75a
Azhad Ghufran <azhad56@gmail.com>
2021-01-09 20:08:23 +0530

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
- 
+
 import sys
 import spotipy
 import spotipy.util as util
@@ -8,19 +8,19 @@ import random
 
 from moodtape_functions import authenticate_spotify, aggregate_top_artists, aggregate_top_tracks, select_tracks, create_playlist
 
-client_id = "blank"
-client_secret = "blank"
-redirect_uri = "blank"
+client_id = "52a54fcd76944df9b560435d1ceb4f37"
+client_secret = "ccbb20f5c45f4122a693594a69419647"
+redirect_uri = "https://localhost:8008/"
 
 scope = 'user-library-read user-top-read playlist-modify-public user-follow-read'
 
 username = "mohithms"
-token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
-
-
+token = util.prompt_for_user_token(
+    username, scope, client_id, client_secret, redirect_uri)
 
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def my_form():
@@ -29,18 +29,18 @@ def my_form():
 
 @app.route("/", methods=['POST'])
 def moodtape():
-	mood = request.form['text']
-	# username = request.form['username']
-	mood = float(mood)
-	# token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
-	spotify_auth = authenticate_spotify(token)
-	top_artists = aggregate_top_artists(spotify_auth)
-	top_tracks = aggregate_top_tracks(spotify_auth, top_artists)
-	selected_tracks = select_tracks(spotify_auth, top_tracks, mood)
-	playlist = create_playlist(spotify_auth, selected_tracks, mood)
-	return render_template('playlist.html', playlist=playlist )
+    mood = request.form['text']
+    # username = request.form['username']
+    mood = float(mood)
+    # token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
+    spotify_auth = authenticate_spotify(token)
+    top_artists = aggregate_top_artists(spotify_auth)
+    top_tracks = aggregate_top_tracks(spotify_auth, top_artists)
+    selected_tracks = select_tracks(spotify_auth, top_tracks, mood)
+    playlist = create_playlist(spotify_auth, selected_tracks, mood)
+    return render_template('playlist.html', playlist=playlist)
 
 
 if __name__ == "__main__":
 
-	app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
